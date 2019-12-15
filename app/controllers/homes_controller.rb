eb9f818809b3ce6dd94e63b61788d2812602bef8
Daichi1022 <1022722522zzz@gmail.com>
2019-12-15 23:59:59 +0900

    COMMIT

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
   def top
   end
-  
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
-
+  
 end
